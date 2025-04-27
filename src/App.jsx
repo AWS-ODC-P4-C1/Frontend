9850595c270e86d9e8f4291b9b5526eaa94d44fd
madicke12 <cissemadicke8@gmail.com>
2025-04-27 21:58:38 +0000

    Remove unnecessary whitespace in App component

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        
         <div className="min-h-screen bg-gray-50">
           <Navbar />
           <div className="container mx-auto px-4 py-8">
