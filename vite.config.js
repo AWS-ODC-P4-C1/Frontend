7f13205d8595f82b2da3b67277aa39c23047c1f3
madicke12 <cissemadicke8@gmail.com>
2025-04-28 09:39:35 +0000

    Fix formatting by aligning the server block in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: resolve(__dirname, 'vitest.setup.js'),
   },
-     server: {
+    server: {
     host: '0.0.0.0', // so it binds to public IP
     port: 5173,      // or whatever port you're using
     allowedHosts: ['ec2-54-191-62-131.us-west-2.compute.amazonaws.com']
